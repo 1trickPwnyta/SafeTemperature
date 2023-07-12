d715c90c014b1bcce3c458503a7fd8667426fa87
Sarah Pace <sarahpace37@gmail.com>
2023-07-12 16:52:05 -0700

    initial commit

---
+++ Source/Patch_JobGiver_SeekSafeTemperature.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace SafeTemperature
+{
+    [HarmonyPatch(typeof(JobGiver_SeekSafeTemperature))]
+    [HarmonyPatch("TryGiveJob")]
+    public static class Patch_JobGiver_SeekSafeTemperature_TryGiveJob
+    {
+        public static void Postfix(Pawn pawn, ref Job __result)
+        {
+            if (pawn.health.hediffSet.HasTemperatureInjury(TemperatureInjuryStage.Initial))
+            {
+                FloatRange tempRange = pawn.ComfortableTemperatureRange();
+                if (tempRange.Includes(pawn.AmbientTemperature))
+                {
+                    __result = JobMaker.MakeJob(JobDefOf.Wait_SafeTemperature, 500, true);
+                }
+            }
+        }
+    }
+}

356207d1497cec8b5d6985315fd6bcb4cb1ea733
1trickPwnyta <sarahpace37@gmail.com>
2024-04-24 11:12:24 -0700

    Fix weird behavior with forbidden doors and mental breaks

--- 1.5/Source/Patch_JobGiver_SeekSafeTemperature.cs
+++ 1.5/Source/Patch_JobGiver_SeekSafeTemperature.cs
@@ -12,55 +12,48 @@ namespace SafeTemperature
     {
         public static void Postfix(Pawn pawn, ref Job __result)
         {
-            // Make sure pawn has at least initial temperature injury before making any calculations
-            if (pawn.health.hediffSet.HasTemperatureInjury(TemperatureInjuryStage.Initial)) 
+            if (__result == null) 
             {
-                FloatRange tempRange = pawn.ComfortableTemperatureRange();
-
-                // Find the safest region for the pawn, starting with the closest safe region
-                Region safestRegion;
-                safestRegion = JobGiver_SeekSafeTemperature.ClosestRegionWithinTemperatureRange(pawn.Position, pawn.MapHeld, pawn, tempRange, TraverseParms.For(pawn));
-
-                // If no safe region exists, find the best region based on whether the pawn is too cold or too hot
-                if (safestRegion == null)
+                // Make sure pawn has at least initial temperature injury before making any calculations
+                if (pawn.health.hediffSet.HasTemperatureInjury(TemperatureInjuryStage.Initial) && !pawn.InMentalState)
                 {
-                    Hediff hypothermia = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Hypothermia);
-                    Hediff heatstroke = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Heatstroke);
+                    FloatRange tempRange = pawn.ComfortableTemperatureRange();
 
-                    if (hypothermia != null && (heatstroke == null || hypothermia.Severity > heatstroke.Severity))
+                    // Find the safest region for the pawn, starting with the closest safe region
+                    Region safestRegion;
+                    safestRegion = JobGiver_SeekSafeTemperature.ClosestRegionWithinTemperatureRange(pawn.Position, pawn.MapHeld, pawn, tempRange, TraverseParms.For(pawn));
+
+                    // If no safe region exists, find the best region based on whether the pawn is too cold or too hot
+                    if (safestRegion == null)
                     {
-                        // Look for the coldest region warmer than comfortable range
-                        safestRegion = TemperatureUtility.ColdestRegionWarmerThanTemperatureRange(pawn.Position, pawn.MapHeld, pawn, tempRange, TraverseParms.For(pawn));
-                        // If no region exists, look for the warmest region
-                        if (safestRegion == null)
+                        Hediff hypothermia = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Hypothermia);
+                        Hediff heatstroke = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Heatstroke);
+
+                        if (hypothermia != null && (heatstroke == null || hypothermia.Severity > heatstroke.Severity))
                         {
-                            FloatRange max = new FloatRange(float.MaxValue);
-                            safestRegion = TemperatureUtility.WarmestRegionColderThanTemperatureRange(pawn.Position, pawn.MapHeld, pawn, max, TraverseParms.For(pawn));
+                            // Look for the coldest region warmer than comfortable range
+                            safestRegion = TemperatureUtility.ColdestRegionWarmerThanTemperatureRange(pawn.Position, pawn.MapHeld, pawn, tempRange, TraverseParms.For(pawn));
+                            // If no region exists, look for the warmest region
+                            if (safestRegion == null)
+                            {
+                                FloatRange max = new FloatRange(float.MaxValue);
+                                safestRegion = TemperatureUtility.WarmestRegionColderThanTemperatureRange(pawn.Position, pawn.MapHeld, pawn, max, TraverseParms.For(pawn));
+                            }
+                        }
+                        if (heatstroke != null && (hypothermia == null || heatstroke.Severity > hypothermia.Severity))
+                        {
+                            // Look for the warmest region colder than comfortable range
+                            safestRegion = TemperatureUtility.WarmestRegionColderThanTemperatureRange(pawn.Position, pawn.MapHeld, pawn, tempRange, TraverseParms.For(pawn));
+                            // If no region exists, look for the coldest region
+                            if (safestRegion == null)
+                            {
+                                FloatRange min = new FloatRange(float.MinValue);
+                                safestRegion = TemperatureUtility.ColdestRegionWarmerThanTemperatureRange(pawn.Position, pawn.MapHeld, pawn, min, TraverseParms.For(pawn));
+                            }
                         }
                     }
-                    if (heatstroke != null && (hypothermia == null || heatstroke.Severity > hypothermia.Severity))
-                    {
-                        // Look for the warmest region colder than comfortable range
-                        safestRegion = TemperatureUtility.WarmestRegionColderThanTemperatureRange(pawn.Position, pawn.MapHeld, pawn, tempRange, TraverseParms.For(pawn));
-                        // If no region exists, look for the coldest region
-                        if (safestRegion == null)
-                        {
-                            FloatRange min = new FloatRange(float.MinValue);
-                            safestRegion = TemperatureUtility.ColdestRegionWarmerThanTemperatureRange(pawn.Position, pawn.MapHeld, pawn, min, TraverseParms.For(pawn));
-                        }
-                    }
-                }
 
-                if (safestRegion == null)
-                {
-                    Debug.Log("Couldn't find the safest region for: " + pawn.Name);
-                }
-
-                // Check if current job is taking the pawn to the safest temperature already
-                if (pawn.CurJob == null || pawn.CurJob.GetTarget(TargetIndex.A) == null || !pawn.CurJob.GetTarget(TargetIndex.A).IsValid || pawn.CurJob.GetTarget(TargetIndex.A).Cell.GetTemperature(pawn.Map) != safestRegion.Room.Temperature)
-                {
-                    // Check if a safe temperature job has already been issued
-                    if (__result == null)
+                    if (safestRegion != null)
                     {
                         if (safestRegion.Cells.ToList().Contains(pawn.Position))
                         {
@@ -80,11 +73,6 @@ namespace SafeTemperature
                             }
                         }
                     }
-                }
-                else
-                {
-                    // If current job leads to safest region, cancel issuing any new job
-                    __result = null;
                 }
             }
         }
--- 1.5/Source/TemperatureUtility.cs
+++ 1.5/Source/TemperatureUtility.cs
@@ -1,4 +1,7 @@
-﻿using Verse;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using Verse.AI;
 
 namespace SafeTemperature
 {
@@ -12,33 +15,23 @@ namespace SafeTemperature
                 return null;
             }
 
+            IEnumerable<Region> validRegions = map.regionGrid.AllRegions.Where(r => IsValidRegionForPawn(r, pawn));
+
             float coldestTemp = float.MaxValue;
-            foreach (Region region in map.regionGrid.AllRegions)
+            foreach (Region region in validRegions)
             {
-                if (!region.IsDoorway && region.Room.Temperature < coldestTemp && region.Room.Temperature > tempRange.max)
+                if (region.Room.Temperature < coldestTemp && region.Room.Temperature > tempRange.max)
                 {
                     coldestTemp = region.Room.Temperature;
                 }
             }
 
-            RegionEntryPredicate entryCondition = (Region from, Region r) => r.Allows(traverseParms, false);
-            Region foundReg = null;
-            RegionProcessor regionProcessor = delegate (Region r)
+            if (!validRegions.Any(r => r.Room.Temperature != coldestTemp))
             {
-                IntVec3 intVec;
-                if (!RimWorld.JobGiver_SeekSafeTemperature.TryGetAllowedCellInRegion(r, pawn, out intVec))
-                {
-                    return false;
-                }
-                if (r.Room.Temperature == coldestTemp)
-                {
-                    foundReg = r;
-                    return true;
-                }
-                return false;
-            };
-            RegionTraverser.BreadthFirstTraverse(rootRegion, entryCondition, regionProcessor);
-            return foundReg;
+                return null;
+            }
+
+            return ClosestRegionInTemperature(rootRegion, coldestTemp, traverseParms, pawn);
         }
 
         public static Region WarmestRegionColderThanTemperatureRange(IntVec3 root, Map map, Pawn pawn, FloatRange tempRange, TraverseParms traverseParms)
@@ -49,25 +42,37 @@ namespace SafeTemperature
                 return null;
             }
 
+            IEnumerable<Region> validRegions = map.regionGrid.AllRegions.Where(r => IsValidRegionForPawn(r, pawn));
+
             float warmestTemp = float.MinValue;
-            foreach (Region region in map.regionGrid.AllRegions)
+            foreach (Region region in validRegions)
             {
-                if (!region.IsDoorway && region.Room.Temperature > warmestTemp && region.Room.Temperature < tempRange.min)
+                if (region.Room.Temperature > warmestTemp && region.Room.Temperature < tempRange.min)
                 {
                     warmestTemp = region.Room.Temperature;
                 }
             }
 
+            if (!validRegions.Any(r => r.Room.Temperature != warmestTemp))
+            {
+                return null;
+            }
+
+            return ClosestRegionInTemperature(rootRegion, warmestTemp, traverseParms, pawn);
+        }
+
+        private static bool IsValidRegionForPawn(Region region, Pawn pawn)
+        {
+            return !region.IsDoorway && RimWorld.JobGiver_SeekSafeTemperature.TryGetAllowedCellInRegion(region, pawn, out IntVec3 intVec) && pawn.CanReach(new LocalTargetInfo(intVec), PathEndMode.OnCell, Danger.Deadly);
+        }
+
+        private static Region ClosestRegionInTemperature(Region rootRegion, float temperature, TraverseParms traverseParms, Pawn pawn)
+        {
             RegionEntryPredicate entryCondition = (Region from, Region r) => r.Allows(traverseParms, false);
             Region foundReg = null;
             RegionProcessor regionProcessor = delegate (Region r)
             {
-                IntVec3 intVec;
-                if (!RimWorld.JobGiver_SeekSafeTemperature.TryGetAllowedCellInRegion(r, pawn, out intVec))
-                {
-                    return false;
-                }
-                if (r.Room.Temperature == warmestTemp)
+                if (IsValidRegionForPawn(r, pawn) && r.Room.Temperature == temperature)
                 {
                     foundReg = r;
                     return true;
